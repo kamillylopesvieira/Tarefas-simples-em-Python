b29cad806a69b181c74f5d1140684aade3b2a20b
Kamilly Lopes <kamillylopesvieira@gmail.com>
2026-08-13 21:42:55 -0300

    EX1.py

---
+++ EX1.py
@@ -0,0 +1,7 @@
+# Exercício 1 — Contador A 
+# Seu programa deve ler um valor inteiro positivo e imprimir, um por linha, os valores entre 0 e o valor imediatamente anterior ao inteiro lido.
+# Resposta
+print('digite um valor:')
+valor = int(input())
+for i in range(0,valor):
+  print(i,)
